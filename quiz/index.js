616879c73c34a7c21953475c929c9405d43e6f32
SebaGut2103 <juansefernando.guti2103@gmail.com>
2024-12-18 20:58:12 -0500

    quiz

---
+++ quiz/index.js
@@ -0,0 +1,10 @@
+const btn = document.querySelector("#btn-play");
+const us1 = document.getElementById("us-1");
+const us2 = document.getElementById("us-2");
+const buttons = document.querySelectorAll("btn");
+
+let points = 0;
+
+btn.addEventListener(  "", (event) =>{
+    e.target.style.backgroundColor = "hotpink"
+})
